f47c9031b185c19f74abf0d62cbc417421b6731f
Crelle490 <christian.clt@gmail.com>
2025-02-14 14:44:31 +0100

    d

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -23,21 +23,21 @@ def generate_launch_description():
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # robot_state_publisher node on topic "robot_description"
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params_robot_state_publisher = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[params_robot_state_publisher]
     )
 
-    params = {'robot_description': robot_description_config}
+    params_joint_state_publisher = {'robot_description': robot_description_config}
     node_joint_state_publisher_gui = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         output='screen',
-        parameters=[params]
+        parameters=[params_joint_state_publisher]
     )
 
 
