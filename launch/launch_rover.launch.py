e924bfb549f0c70ec97bd82863f2f7f37828331e
Crelle490 <christian.clt@gmail.com>
2025-02-18 14:25:50 +0100

    d

--- launch/launch_rover.launch.py
+++ launch/launch_rover.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
     robot_description = Command([
         'ros2 param get --hide-type /robot_state_publisher robot_description'
     ])
-    controller_params = os.path.join(get_package_share_directory(package_name), 'config', 'my_trajectory_controller.yaml')
+    controller_params = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
