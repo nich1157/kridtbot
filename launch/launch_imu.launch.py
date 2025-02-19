e5d1ee103e49558cc8b10a38c0cd00248ea1f77d
Crelle490 <christian.clt@gmail.com>
2025-02-19 13:29:02 +0100

    d

--- launch/launch_imu.launch.py
+++ launch/launch_imu.launch.py
@@ -1,21 +1,19 @@
-from launch_ros.substitutions import FindPackageShare
-
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
-
-
+from launch_ros.actions import Node
 def generate_launch_description():
-
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('bno055'),
-                    'launch',
-                    'bno055.launch.py'
-                ])
-            ])
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('bno055'),
+        'config',
+        'bno055_params.yaml'
         )
-    ])
+        
+    node=Node(
+        package = 'bno055',
+        executable = 'bno055',
+        parameters = [config]
+    )
+    ld.add_action(node)
+    return ld
